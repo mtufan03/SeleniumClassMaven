5b33adebbddaff009c0f1bbc7cfa42f9363cfe2e
Ugureroglu2000 <ugureroglu2000@gmail.com>
2021-04-08 02:01:54 +0100

    pomm

--- src/test/java/SelniumTests/Pages/CartPage.java
+++ src/test/java/SelniumTests/Pages/CartPage.java
@@ -11,7 +11,7 @@ public class CartPage {
     @FindBy(className = "inventory_item_name")
     public List<WebElement> ItemsList;
     @FindBy(css = ".btn.btn_secondary.btn_small.btn_inventory")
-    public List<WebElement> removeButtons;
+    public List<WebElement> RemoveButtons;
     @FindBy(className = "inventory_item_price")
     public List<WebElement> ItemsPriceList;
     @FindBy (id="continue-shopping")
@@ -19,6 +19,23 @@ public class CartPage {
     @FindBy (xpath = "checkout")
     public WebElement checkout;
 
+    public  void remove(String str){
+        for( int i=0; i<ItemsList.size(); i++){
+            if(ItemsList.get(i).getText().contains(str)){
+                RemoveButtons.get(i).click();
+            }
+        }
+    }
+
+    public int getPrice(String str){
+        int a=0;
+        for( int i=0; i<ItemsList.size(); i++){
+            if(ItemsList.get(i).getText().contains(str)){
+                a=Integer.parseInt(ItemsPriceList.get(i).getText());
+            }
+        }
+        return a;
+    }
 
 
 }
